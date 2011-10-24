d22eea4000a785ace3c3dd049ef545fe0846fdf2
Roman Simecek <roman.simecek@cyt.ch>
2011-10-24 08:27:41 +0200

    At the moment the travian bot gets to be one fat class.

--- lib/travian_bot/application.rb
+++ lib/travian_bot/application.rb
@@ -1,37 +1,61 @@
 require 'selenium-webdriver'
-require 'travian_bot/tools'
+require 'chronic'
+require 'yaml'
 
 class TravianBot
   class Application
     class << self
-      include Tools
             
       def run!(*arguments)
+        login
+        building_queue
+        close
+      
+        return 1
+      end
+      
+      private
+      
+      def login
         url, user, password = get_credentials
-        driver = Selenium::WebDriver.for :firefox
-        driver.navigate.to(url)
-
-        # Login
-        name_input = driver.find_element(:name, 'name')
-        password_input = driver.find_element(:name, 'password')
-        login_button = driver.find_element(:id, 's1')
+        @driver = Selenium::WebDriver.for :firefox
+        @driver.navigate.to(url)
+        name_input = @driver.find_element(:name, 'name')
+        password_input = @driver.find_element(:name, 'password')
+        login_button = @driver.find_element(:id, 's1')
 
         name_input.send_keys(user)
         password_input.send_keys(password)
         login_button.submit
-
-        # Check if building something
-        begin
-          building_pipe = driver.find_element(:id, 'building_contract')
-          building_time = driver.find_element(:class, 'buildingTime').text
-          puts wait_till(building_time)
-        rescue Selenium::WebDriver::Error::NoSuchElementError
-          puts 'Nothing to build'
-        end
-
-        driver.quit
+      end
       
-        return 1
+      def close
+        @driver.quit
+      end
+      
+      def building_queue
+        # Check if building something
+        #begin
+          building_pipe = @driver.find_elements(:xpath, "//table[@id='building_contract']/tbody/tr")
+          
+          building_pipe.each do |entry|
+            puts entry.text
+          end
+        #rescue Selenium::WebDriver::Error::NoSuchElementError
+          #puts 'Nothing to build'
+        #end
+      end
+      
+      def get_credentials
+        credentials = YAML::load(File.open("#{ENV['HOME']}/.travian_bot"))
+
+        [credentials['travian_bot']['url'], credentials['travian_bot']['usr'], credentials['travian_bot']['pwd']]
+      end
+
+      def wait_till(input)
+        time = input.to_s.match(/(\d*):(\d*)$/)
+
+        Chronic.parse("today #{time[0]}:#{time[1]}")      
       end
     end
   end
